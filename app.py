bda061e6f7a7047916557ae2375ef7209f9ddaf3
Rajat Singh <rajatsinghten@gmail.com>
2025-03-29 23:54:30 +0530

    port

--- app.py
+++ app.py
@@ -297,5 +297,7 @@ def index():
         return render_template('chat.html')
     return render_template('login.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
