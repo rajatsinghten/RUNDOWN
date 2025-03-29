eb72e65761b3b7824f70658349ba06c738a67190
Rajat Singh <rajatsinghten@gmail.com>
2025-03-30 00:07:55 +0530

    render port change

--- app.py
+++ app.py
@@ -298,6 +298,5 @@ def index():
     return render_template('login.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
